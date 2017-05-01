2f58648e9e5b8a6017387bc4486b0649cd51bdfb
Felipe Rojos <felipe.rojos@comunidadfeliz.cl>
2017-04-30 22:56:19 -0300

    fix migration

--- db/migrate/20170430173651_create_payments.rb
+++ db/migrate/20170430173651_create_payments.rb
@@ -1,7 +1,7 @@
 class CreatePayments < ActiveRecord::Migration[5.0]
   def change
     create_table :payments do |t|
-      t.float :amount
+      t.float :amount, default: 0
       t.references :user, foreign_key: true
 
       t.timestamps
