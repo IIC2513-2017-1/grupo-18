1bbe824882e35c3a67c0efd6da0deed7b3ec728a
Félix <felix91gr@gmail.com>
2017-05-10 18:42:42 -0300

    Added basic bcrypt usage

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,27 +18,30 @@
 #
 
 class User < ApplicationRecord
-  require 'digest/sha2'
-  require 'securerandom'
-  attr_accessor :password
-  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
-  validates :email, presence: true, uniqueness: true, format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates_presence_of :password, on: :create
-  validates_length_of :password, minimum: 4, allow_blank: true
-  validates :password, confirmation: true # password_confirmation attr
-  before_save :encrypt_password
-  after_save :clear_password
+
+  has_secure_password
+
+  # require 'digest/sha2'
+  # require 'securerandom'
+  # attr_accessor :password
+  # validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+  # validates :email, presence: true, uniqueness: true, format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  # validates_presence_of :password, on: :create
+  # validates_length_of :password, minimum: 4, allow_blank: true
+  # validates :password, confirmation: true # password_confirmation attr
+  # before_save :encrypt_password
+  # after_save :clear_password
   has_many :user_bets
   has_many :payments
-  validates :balance, :numericality => { :greater_than_or_equal_to => 0}
+  # validates :balance, :numericality => { :greater_than_or_equal_to => 0}
 
   #TODO: Salt using secure random and
-  def encrypt_password
-    return unless password.present?
-    self.encrypted_password = Digest::SHA256.hexdigest(password)
-  end
+  # def encrypt_password
+  #   return unless password.present?
+  #   self.encrypted_password = Digest::SHA256.hexdigest(password)
+  # end
 
-  def clear_password
-    self.password = nil
-  end
+  # def clear_password
+  #   self.password = nil
+  # end
 end
