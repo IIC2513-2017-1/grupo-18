ec4e8b28be00724d10b9ef8ed0527a541275d7ea
Félix <felix91gr@gmail.com>
2017-05-14 12:39:09 -0400

    Added blocking behavior for when user is not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  before_action :set_locale
+  before_action :set_locale, :check_logged_in
 
   def set_locale
     I18n.locale = 'es'
   end
+
+  private
+
+	  def check_logged_in
+    	unless logged_in
+	  		flash[:error] = "You must be logged in to access this page"
+      		redirect_to login_url
+	  	end
+	  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  skip_before_action :check_logged_in, only: [:new, :create]
+
+
   def new
   end
 
