67999b58cba3b58f8086a0eacaf3e2ad9f29994f
Félix <felix91gr@gmail.com>
2017-05-14 13:22:43 -0400

    Linked Login to SignUp. Now you can LogIn and SignUp, even when you are not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  skip_before_action :check_logged_in, only: [:new, :create]
+
   # GET /users
   # GET /users.json
   def index
