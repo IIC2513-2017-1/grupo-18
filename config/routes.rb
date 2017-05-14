6d202e0183f7531d6f6be1dc5b7ca7b2576b60dc
Félix <felix91gr@gmail.com>
2017-05-13 22:49:19 -0300

    Added login and sessions. Added helpers for logout.
    TODO: change layout depending on session state

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :user_bets
   resources :bet_options
   resources :payments
   resources :bets
   resources :users
   get '/', to: 'home#index'
+
+  # Sessions  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
