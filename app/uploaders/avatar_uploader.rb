066d9c352f10b33161a67a379b1eaa72b35ecc13
Felipe Rojos <felipe.rojos@comunidadfeliz.cl>
2017-05-28 17:58:04 -0400

    a href to home bets

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -34,7 +34,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [150, 150]
   end
   version :medium do
-    process resize_to_fit: [300, 300]
+    process resize_to_fit: [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
