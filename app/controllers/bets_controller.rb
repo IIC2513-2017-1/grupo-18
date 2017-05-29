f7f7806b63f7efeede4e0b2d256fa2031e755bb5
Felipe Rojos <felipe.rojos@comunidadfeliz.cl>
2017-05-28 23:45:46 -0400

    update bets controller

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -49,11 +49,12 @@ class BetsController < ApplicationController
     aux[:bet][:bet_options_attributes] = params[:bet_options]
     aux[:bet][:bet_options_attributes].delete("{index}")
     aux.delete("bet_options")
-    aux = aux.require(:bet).permit(:execution_date, :description, bet_options_attributes: [:description, :percentage, :win])
+    aux = aux.require(:bet).permit(:execution_date, :description, bet_options_attributes: [:description, :percentage, :win, :id])
     @bet = Bet.new(aux)
     @bet.user = current_user
     respond_to do |format|
       if @bet.save
+
         format.html { redirect_to @bet, notice: 'Bet was successfully created.' }
         format.json { render :show, status: :created, location: @bet }
       else
@@ -71,6 +72,7 @@ class BetsController < ApplicationController
     aux[:bet][:bet_options_attributes].delete("{index}")
     aux.delete("bet_options")
     aux = aux.require(:bet).permit(:execution_date, :description, bet_options_attributes: [:description, :percentage, :win, :id])
+    abort
     respond_to do |format|
       if @bet.update(aux)
 
