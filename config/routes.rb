7306fc5f9e4980464fdf62b52bee4067dc17b825
Felipe Rojos <felipe.rojos@comunidadfeliz.cl>
2017-06-10 15:59:22 -0400

    password field

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # Sessions
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get '/logout',  to: 'sessions#destroy'
 
   # Navbar paths
   root 'home#index'
