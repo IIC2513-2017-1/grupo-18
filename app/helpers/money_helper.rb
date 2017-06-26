2c26005982968fe21bcd534cf3f9fda4e96f1ba7
felix91gr <felix91gr@gmail.com>
2017-06-26 19:21:19 -0400

    Added international currency display to header.

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -3,29 +3,37 @@ module MoneyHelper
 	def get_uf
 		a = HTTParty.get('http://www.mindicador.cl/api/uf', :headers => hash1)
 		uf = a.parsed_response['serie'].first["valor"]
+		format_currency(uf)
 	end
 	
 	def get_dolar
 		uri = URI('http://www.mindicador.cl/api/dolar')
 		a = Net::HTTP.get(uri)
 		a = eval(a)
-		dolar = a[:serie].first
+		dolar = a[:serie].first[:valor]
+		format_currency(dolar)
 	end
 
 	def get_euro
 		a = HTTParty.get('http://www.mindicador.cl/api/euro', :headers => hash1)
-		euro = a.parsed_response['serie'].first
+		euro = a.parsed_response['serie'].first["valor"]
+		format_currency(euro)
 	end
 
 	def get_utm
 		a = HTTParty.get('http://www.mindicador.cl/api/utm', :headers => hash1)
-		utm = a.parsed_response['serie'].first
+		utm = a.parsed_response['serie'].first["valor"]
+		format_currency(utm)
 	end
 
 	private
 
 		def hash1
 			{ 'Content-type' => 'application/json; charset=utf-8' }
+		end
+
+		def format_currency(value)
+			number_to_currency(value, unit: "CLP", precision: 0)
 		end
 
 end
